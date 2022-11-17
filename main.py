0794589d8c5a20f3d76f087e84cff9cd9cb8a8ca
BronzonTech <60158978+BronzonTech-Cloud@users.noreply.github.com>
2022-11-17 20:35:01 +0000

    Create main.py
    Add

---
+++ main.py
@@ -0,0 +1,138 @@
+import pyfiglet
+
+print("               ############################################################      ")
+print("               #                                                          #      ")
+print("               #                   +-+-+-+-+-+-+-+-+-+-+-+                #      ")
+print("               #                   |B|r|o|n|z|o|n|T|e|c|h|                #      ")
+print("               #                   +-+-+-+-+-+-+-+-+-+-+-+                #      ")
+print("               #                                                          #      ")
+print("               #                                      ₿ṜѲ₦ⱿѲ₦₸ĒĦ₵Ħ        #      ")
+print("               #                                                          #      ")
+print("               ############################################################      ")
+
+vivi = input("Entrer votre texte :")               #demande du texte
+print("Choisissez votre font en se rendant sur ce site www.figletfy.66ghz.com ") #demande de choisir le font 
+vald = int(input("Entrer le numero de votre font retouver sur le site: "))
+def Next_Setup(): 
+    if vald == 1 :
+        a = pyfiglet.figlet_format(vivi,font='3-d')
+        print(a)
+    if vald == 2 :
+        b = pyfiglet.figlet_format(vivi, font='3x5')
+        print(b)
+    if vald == 3 :
+        c = pyfiglet.figlet_format(vivi,font='5lineoblique')
+        print(c)
+    if vald == 4 :
+        d = pyfiglet.figlet_format(vivi,font='acrobatic')
+        print(d)
+    if vald == 5 :
+        e = pyfiglet.figlet_format(vivi,font='alligator')
+        print(e)
+    if vald == 6 :
+        f = pyfiglet.figlet_format(vivi,font='alligator2')
+        print(f)
+    if vald == 7 :
+        g = pyfiglet.figlet_format(vivi,font='alphabet')
+        print(g)
+    if vald == 8 :
+        h = pyfiglet.figlet_format(vivi,font='avatar')
+        print(h)
+    if vald == 9 :
+        i = pyfiglet.figlet_format(vivi,font='banner')
+        print(i)
+    if vald == 10 :
+        j = pyfiglet.figlet_format(vivi,font='banner3-D')
+        print(j)
+    if vald == 11 :
+        k = pyfiglet.figlet_format(vivi,font='banner3')
+        print(k)
+    if vald == 12 :
+        l = pyfiglet.figlet_format(vivi,font='banner4')
+        print(l)
+    if vald == 13 :
+        m = pyfiglet.figlet_format(vivi,font='barbwire')
+        print(m)
+    if vald == 14 :
+        n = pyfiglet.figlet_format(vivi,font='basic')
+        print(n)
+    if vald == 15 :
+        o = pyfiglet.figlet_format(vivi,font='bell')
+        print(o)
+    if vald == 16 :
+        p = pyfiglet.figlet_format(vivi,font='big')
+        print(p)
+    if vald == 17 :
+        q = pyfiglet.figlet_format(vivi,font='bigchief')
+        print(q)
+    if vald == 18 :
+        r = pyfiglet.figlet_format(vivi,font='binary')
+        print(r)
+    if vald == 19 :
+        s = pyfiglet.figlet_format(vivi,font='block')
+        print(s)
+    if vald == 20 :
+        t = pyfiglet.figlet_format(vivi,font='bubble')
+        print(t)
+    if vald == 21 :
+        u = pyfiglet.figlet_format(vivi,font='bulbhead')
+        print(u)
+    if vald == 22 :
+        v = pyfiglet.figlet_format(vivi,font='calgphy2')
+        print(v)
+    if vald == 23 :
+        w = pyfiglet.figlet_format(vivi,font='caligraphy')
+        print(w)
+    if vald == 24 :
+        x = pyfiglet.figlet_format(vivi,font='catwalk')
+        print(x)
+    if vald == 25 :
+        y = pyfiglet.figlet_format(vivi,font='chunky')
+        print(y)
+    if vald == 26 :
+        z = pyfiglet.figlet_format(vivi,font='coinstak')
+        print(z)
+    if vald == 27 :
+        aa = pyfiglet.figlet_format(vivi,font='colossal')
+        print(aa)
+    if vald == 28 :
+        bb = pyfiglet.figlet_format(vivi,font='computer')
+        print(bb)
+    if vald == 29 :
+        cc = pyfiglet.figlet_format(vivi,font='contessa')
+        print(cc)
+    if vald == 30 :
+        dd = pyfiglet.figlet_format(vivi,font='contrast')
+        print(dd)
+    if vald == 31 :
+        ee = pyfiglet.figlet_format(vivi,font='cosmic')
+        print(ee)
+    if vald == 32 :
+        ff = pyfiglet.figlet_format(vivi,font='cosmike')
+        print(ff)
+    if vald == 33 :
+        gg = pyfiglet.figlet_format(vivi,font='cricket')
+        print(gg)
+    if vald == 34 :
+        hh = pyfiglet.figlet_format(vivi,font='cyberlarge')
+        print(hh)
+    if vald == 35 :
+        ii = pyfiglet.figlet_format(vivi,font='cybermedium')
+        print(ii)
+    if vald == 36 :
+        jj = pyfiglet.figlet_format(vivi,font='cybersmall')
+        print(jj)
+    if vald == 37 :
+        kk = pyfiglet.figlet_format(vivi,font='diamomd')
+        print(kk)
+    if vald == 38 :
+        ll = pyfiglet.figlet_format(vivi,font='digital')
+        print(ll)
+    if vald == 39 :
+        mm = pyfiglet.figlet_format(vivi,font='doh')
+        print(mm)
+    if vald == 40 :
+        nn = pyfiglet.figlet_format(vivi,font='doom')
+        print(nn)
+    
+Next_Setup()
